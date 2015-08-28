bbe187eb8f3c3e7ab5019317facda29fb53ec5df
Rosstin <asterazul@gmail.com>
2015-08-28 13:54:39 -0700

    added fake messaging, fake messaging doesnt crash

---
+++ monstr/MonstrMessagesViewController.h
@@ -0,0 +1,38 @@
+//
+//  MonstrMessagesViewController.h
+//  Monstr
+//
+//  Created by Rosstin Murphy on 8/28/15.
+//  Copyright (c) 2015 Richard Kim. All rights reserved.
+//
+
+#import "JSQMessages.h"
+
+#import "DemoModelData.h"
+#import "NSUserDefaults+DemoSettings.h"
+
+#import "JSQMessagesViewController.h"
+
+#import "JSQMessagesComposerTextView.h"
+
+@class MonstrMessagesViewController;
+
+@protocol JSQMonstrViewControllerDelegate <NSObject>
+
+- (void)didDismissJSQMonstrViewController:(MonstrMessagesViewController *)vc;
+
+@end
+
+
+@interface MonstrMessagesViewController : JSQMessagesViewController <UIActionSheetDelegate>//, JSQMessagesComposerTextViewPasteDelegate>
+
+@property (weak, nonatomic) id<JSQMonstrViewControllerDelegate> delegateModal;
+
+@property (strong, nonatomic) DemoModelData *demoData;
+
+-(void) receiveMessagePressed:(UIBarButtonItem *)sender;
+
+-(void)closePressed:(UIBarButtonItem *)sender;
+
+
+@end
